12b237257be20152c6de364951f00d62c78aa265
ghassanelarabed <garabeed@gmail.com>
2022-10-31 17:57:59 +0200

    game project

---
+++ ng-video-game-db/src/app/components/home/home.component.ts
@@ -0,0 +1,60 @@
+import { HttpService } from './../../services/http.service';
+import { Game, APIResponse } from './../../models';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import {Subscription} from 'rxjs'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit,OnDestroy {
+  public sort:any;
+  public games?:Array<Game>;
+  private routeSub?:Subscription;
+  private gameSub?:Subscription;
+
+  constructor(
+    private httpService:HttpService,
+    private router:Router,
+    private activatedRoute: ActivatedRoute
+
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params:Params)=>
+    {
+      if (params['game-search']){
+
+        this.searchGames('metacrit',params['game-search']);
+      }else{
+        this.searchGames('metacrit')
+      }
+    });
+  }
+  searchGames(sort: string,search?:string):void{
+    this.httpService.getGameList(sort,search)
+    .subscribe((gameList:APIResponse<Game>)=>{
+      this.games=gameList.results;
+      console.log(gameList)
+    });
+
+  }
+
+
+  openGameDetails(id: string): void{
+    this.router.navigate(['details',id]);
+  }
+
+  ngOnDestroy(): void{
+    if(this.gameSub){
+      this.gameSub.unsubscribe();
+
+    }
+    if(this.routeSub){
+      this.routeSub.unsubscribe();
+    }
+
+  }
+}
